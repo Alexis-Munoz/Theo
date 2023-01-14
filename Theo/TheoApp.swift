b018e6b560ed728d7564ae81bb6e1c0700165731
alexis-munoz <memes.jasper.0k@icloud.com>
2023-01-13 21:54:05 -0600

    Initial Commit

---
+++ Theo/TheoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheoApp.swift
+//  Theo
+//
+//  Created by Alexis Muñoz on 1/13/23.
+//
+
+import SwiftUI
+
+@main
+struct TheoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
