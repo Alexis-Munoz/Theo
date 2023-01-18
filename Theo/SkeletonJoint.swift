fa226f3b417ca1d4fee296351a74772eb21df7a9
alexis-munoz <memes.jasper.0k@icloud.com>
2023-01-18 12:27:23 -0600

    initialize

---
+++ Theo/SkeletonJoint.swift
@@ -0,0 +1,13 @@
+//
+//  SkeletonJoint.swift
+//  Theo
+//
+//  Created by Alexis Muñoz on 1/15/23.
+//
+
+import Foundation
+
+struct SkeletonJoint {
+    let name: String
+    var position: SIMD3<Float>
+}
